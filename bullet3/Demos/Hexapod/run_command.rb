fc7f24d253aa7fc14b453c67abbc2487b09421f2
Shipeng Xu <billhsu.x@gmail.com>
2015-05-17 15:18:34 +0800

    add run_command.rb

---
+++ bullet3/Demos/Hexapod/run_command.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+#Shipeng Xu
+require 'socket'
+filename = ARGV[0]
+sock = TCPSocket.new('localhost', 5555)
+line_num=0
+text=File.open(filename).read
+text.gsub!(/\r\n?/, "\n")
+text.each_line do |line|
+  line=line.gsub(/T.*$/, "")
+  print "#{line_num += 1} #{line}"
+  sock.write line
+  sleep 2
+end
+sock.close
